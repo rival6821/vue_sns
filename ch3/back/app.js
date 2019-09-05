3b760e1ca6d23d12ec57749ed275a94b4b69d3ae
ilhoon <rival6821@naver.com>
2019-09-05 22:08:57 +0900

    백앤드 작업 준비

---
+++ ch3/back/app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("안녕!");
+});
+
+app.listen(3085, () => {
+  console.log(`http://localhost:3085 번 포트에서 작동중`);
+});
--- ch3/front/nuxt.config.js
+++ ch3/front/nuxt.config.js
@@ -1,17 +1,9 @@
 module.exports = {
-    head: {
-        title: 'Vue-SNS'
-    },
-    modules: [
-        '@nuxtjs/axios',
-    ],
-    devModules: [
-        '@nuxtjs/vuetify',
-    ],
-    plugins: [
-
-    ],
-    vuetify: {
-
-    }
+  head: {
+    title: "Vue-SNS"
+  },
+  modules: ["@nuxtjs/axios"],
+  buildModules: ["@nuxtjs/vuetify"],
+  plugins: [],
+  vuetify: {}
 };
